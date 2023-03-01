5f32a38011c5d795db031cdfba73511754120cb9
lsc132428 <lsc132428@stud.gibb.ch>
2023-03-01 17:46:32 +0100

    updated chat

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, request
+import datetime
+
 import openai
-import os
+from flask import Flask, render_template, request
 
 # Set up OpenAI API key
 openai.api_key_path = 'openAIKey'
 
 # Create Flask app
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder='templates', static_folder='static')
 # Keep track of chat history
 chat_history = []
+
 
 # Define home page route
 @app.route('/', methods=['GET', 'POST'])
@@ -19,7 +20,8 @@ def home():
         user_input = request.form['message']
 
         # Add user input to chat history
-        chat_history.append({'speaker': 'User', 'text': user_input})
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        chat_history.append({'speaker': 'User', 'text': user_input, 'time': current_time})
 
         # Generate response using OpenAI
         prompt = ""
@@ -40,18 +42,21 @@ def home():
             chatbot_response = response.choices[0].text.strip()
 
             # Add chatbot response to chat history
+            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             chat_history.append({'speaker': 'Chatbot', 'text': chatbot_response})
 
         except openai.ErrorObject as e:
             # Handle API errors
             chatbot_response = f"Error: {e}"
-            chat_history.append({'speaker': 'Chatbot', 'text': chatbot_response})
+            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            chat_history.append({'speaker': 'Chatbot', 'text': chatbot_response, 'time': current_time})
 
         # Render home page with chat history
         return render_template('index.html', chat_history=chat_history)
 
     # Render home page
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
