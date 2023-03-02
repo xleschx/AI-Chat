65e4252b0a41c30d13576ce32a22555aab04b39c
lsc132428 <lsc132428@stud.gibb.ch>
2023-03-02 10:56:10 +0100

    updated app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import datetime
-import os
 
 import openai
 from flask import Flask, render_template, request
@@ -60,5 +59,5 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True)
 
